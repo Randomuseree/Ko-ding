2201d8cfc53972f289700f67df4cb002a52a0d04
Randomuseree <azembrzuski473@technikumkreatywne.pl>
2024-11-21 13:21:17 +0100

    Add files via upload

---
+++ Liczba_puzzle.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout << "Liczba do rozkladu: ";
+    cin >> a;
+    b = 2;
+    while (a > 1) {
+        while (a % b == 0) {
+            cout << "Liczba pierwsza znaleziona: " << b << endl;
+            a /= b;
+       }
+        b++;
+    }
+    return 0;
+}
